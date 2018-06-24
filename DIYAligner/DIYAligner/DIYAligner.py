b8584e56c7d8fb5249b5bdf7af179590fe1d6b04
Luka Filipovic <luka.filipov95@gmail.com>
2018-06-24 21:18:35 +0200

    complete part 1

--- DIYAligner/DIYAligner/DIYAligner.py
+++ DIYAligner/DIYAligner/DIYAligner.py
@@ -1,1 +1,37 @@
+class Aligner():
 
+    def __init__(self):
+        self.complements = {'A' : 'T', 
+                            'C' : 'G', 
+                            'G' : 'C', 
+                            'T' : 'A'}
+
+    def parse_fasta(self, filename):
+        reference = ""
+        with open(filename, "r") as file:
+            file.readline()
+            reference = file.readline()
+        return reference
+
+    def parse_fastq(self, filename):
+        reads = []
+        with open(filename, "r") as file:
+            while True:
+                 first_line = f.readline()
+                 if len(first_line) == 0:
+                     break
+                 name = first_line[1:].rstrip()
+                 seq = f.readline().rstrip()
+                 f.readline()
+                 qual = f.readline().rstrip()
+                 reads.append((name, seq, qual))
+        return reads
+
+    def complement_reads(self, reads):
+        reads_comp = []
+        for read in reads:
+            comp_read = ""
+            for letter in read:
+                comp_read += self.complements[letter]
+            reads_comp.append((read[0], comp_read, read[2]))
+        return reads_comp
