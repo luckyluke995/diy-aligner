0dfdb78d830e93e0a538c5e30a26247695312f08
Luka Filipovic <luka.filipov95@gmail.com>
2018-06-28 23:22:38 +0200

    create seeds

--- DIYAligner/DIYAligner/DIYAligner.py
+++ DIYAligner/DIYAligner/DIYAligner.py
@@ -1,3 +1,6 @@
+import sys
+import numpy as np
+
 class Aligner():
 
     def __init__(self):
@@ -5,16 +8,16 @@ class Aligner():
                             'C' : 'G', 
                             'G' : 'C', 
                             'T' : 'A'}
+        self.field_size = -1
 
-    def parse_fasta(self, filename):
-        reference = ""
+    def parse_fasta(self, filename, field_length):
+        self.reference = ""
         with open(filename, "r") as file:
             file.readline()
-            reference = file.readline()
-        return reference
+            self.reference = file.readline()
 
     def parse_fastq(self, filename):
-        reads = []
+        self.reads = []
         with open(filename, "r") as file:
             while True:
                  first_line = f.readline()
@@ -24,14 +27,42 @@ class Aligner():
                  seq = f.readline().rstrip()
                  f.readline()
                  qual = f.readline().rstrip()
-                 reads.append((name, seq, qual))
-        return reads
+                 self.reads.append((name, seq, qual))
 
     def complement_reads(self, reads):
-        reads_comp = []
+        self.reads_comp = []
         for read in reads:
             comp_read = ""
             for letter in read:
-                comp_read += self.complements[letter]
-            reads_comp.append((read[0], comp_read, read[2]))
-        return reads_comp
+                comp_read = self.complements[letter] + comp_read
+            self.reads_comp.append((read[0], comp_read, read[2]))
+
+    def create_index(self, ln):
+        self.index = {}
+        self.field_size = ln
+        for i in range(len(self.reference) - ln + 1):
+            substr = self.reference[i:i+ln]
+            if substr in self.index:
+                self.index[substr].append(i)
+            else:
+                self.index[substr] = [i]
+
+    def create_seeds(self, read, length, interval):
+        seeds = []
+        pos = 0
+        while True:
+            if len(read) - pos < length:
+                break
+            seeds.append(read[i:i+length])
+            pos += interval
+        return seeds  
+
+    def seed(self, read, lenght, interval):
+        if self.field_size == -1 or self.field_size != length:
+            create_index(length)
+        seeds = create_seeds(read, lenght, interval)
+        seed_hits = []
+        for seed in seeds:
+            seed_hits.append(self.index[seed])
+        
+
