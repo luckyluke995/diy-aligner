003196f49f5816fd043aee33c84c9776d0ded893
Luka Filipovic <luka.filipov95@gmail.com>
2018-06-29 15:22:36 +0200

    add local aligment

--- DIYAligner/DIYAligner/DIYAligner.py
+++ DIYAligner/DIYAligner/DIYAligner.py
@@ -85,5 +85,64 @@ class Aligner():
         max_neighbours = max_neighbours.sort()
         extend_location = statistics.median(max_neighbours)
         return extend_location
-       
+    
+    def scoring_matrix(a,b):
+        if a==b: return 1 
+        if a=='_' or b=='_': return -7 
+        return -4 
+
+    def local_alignment(x, y, s):
+        D = numpy.zeros((len(x) + 1,len(y) + 1), dtype=int)
+
+        for i in range(1,len(x) + 1):
+            for j in range(1,len(y) + 1):
+                D[i,j] = max(0, 
+                       D[i - 1,j] + s(x[i - 1], '_'),
+                       D[i,j - 1] + s('_',   y[j - 1]), 
+                       D[i - 1,j - 1] + s(x[i - 1],y[j - 1]))
+    
+        local_max = D.max()
+        return D, local_max
+
+    def tracebak(x,y,V, s):
+        maxValue=numpy.where(V==V.max())
+        i=maxValue[0][0]
+        j=maxValue[1][0]
+        ax,ay,am, tr = '','','',''
+        while (i>0 or j>0) and V[i,j]!=0:
+            if i>0 and j>0:
+                sigma = 1 if x[i-1]==y[j-1] else 0
+                d=V[i-1,j-1]+s(x[i-1],y[j-1]) # diagonal move
+            if i>0: v=V[i-1,j]+s(x[i-1],'_')  # vertical move
+            if j>0: h=V[i,j-1]+s('_',y[j-1])  # horizontal move
+        
+            # diagonal is the best
+            if d>=v and d>=h:
+                ax += x[i-1]
+                ay += y[j-1]
+                if sigma==1:
+                    tr+='M'
+                    am+='|'
+                else:
+                    tr+='R'
+                    am+=' '
+                i-=1
+                j-=1
+            # vertical is the best
+            elif v>=h:
+                ax+=x[i-1]
+                ay+='_'
+                tr+='I'
+                am+=' '
+                i-=1
+            # horizontal is the best
+            else:
+                ay+=y[j-1]
+                ax+='_'
+                tr+='D'
+                am+=' '
+                j-=1
+        alignment= '\n'.join([ax[::-1], am[::-1], ay[::-1]])
+        return alignment, tr[::-1]
+
 
